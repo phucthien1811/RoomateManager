a8a9c0a333aede4532a961f692e241de24ded3f0
thientiy1811 <jack0baohbocon@gmail.com>
2026-04-22 10:27:10 +0700

    feat :add port for domain

--- server/src/index.js
+++ server/src/index.js
@@ -22,6 +22,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Roommate Manager API is running' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
